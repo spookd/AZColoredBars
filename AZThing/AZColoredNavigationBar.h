ffb5226a86c6eae4583d0a310054a5c8ddc90419
Alan Zeino <alan.zeino@gmail.com>
2013-09-23 16:00:06 +1000

    Initial Commit.

---
+++ AZThing/AZColoredNavigationBar.h
@@ -0,0 +1,17 @@
+//
+//  AZColoredNavigationBar.h
+//  AZColoredNavigationBar
+//
+//  Created by Alan on 23/09/13.
+//  Copyright (c) 2013 Alan Zeino. All rights reserved.
+//
+
+//
+// Created by combining answers here: http://stackoverflow.com/questions/18897485/achieving-bright-vivid-colors-for-an-ios-7-translucent-uinavigationbar#_=_ and here: https://gist.github.com/aprato/6631390
+//
+// Use with UINavigationController's initWithNavigationBarClass:toolbarClass: method.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZColoredNavigationBar : UINavigationBar @end
---
+++ AZThing/AZColoredNavigationBar.m
@@ -0,0 +1,64 @@
+//
+//  AZColoredNavigationBar.m
+//  AZColoredNavigationBar
+//
+//  Created by Alan on 23/09/13.
+//  Copyright (c) 2013 Alan Zeino. All rights reserved.
+//
+
+#import "AZColoredNavigationBar.h"
+
+@interface AZColoredNavigationBar ()
+
+@property (strong, nonatomic) CALayer *extraColorLayer;
+
+@end
+
+static CGFloat const kDefaultColorLayerOpacity = .7f;
+static CGFloat const kSpaceToCoverStatusBars = 64.f;
+
+@implementation AZColoredNavigationBar
+
+#pragma mark - Instance
+
+- (CALayer *)extraColorLayer
+{
+    if (_extraColorLayer)
+        return _extraColorLayer;
+    
+    _extraColorLayer = [CALayer layer];
+    _extraColorLayer.opacity = kDefaultColorLayerOpacity;
+    [self.layer addSublayer:_extraColorLayer];
+    
+    return _extraColorLayer;
+}
+
+#pragma mark - UIView
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    if ([[[UIDevice currentDevice] systemVersion] integerValue] < 7)
+        return;
+    
+    if (self.extraColorLayer != nil)
+        self.extraColorLayer.frame = CGRectMake(0, 0 - kSpaceToCoverStatusBars, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + kSpaceToCoverStatusBars);
+    
+    [self.extraColorLayer removeFromSuperlayer];
+    [self.layer insertSublayer:_extraColorLayer atIndex:1];
+}
+
+#pragma mark - UINavigationBar
+
+- (void)setBarTintColor:(UIColor *)barTintColor
+{
+    [super setBarTintColor:barTintColor];
+    
+    if ([[[UIDevice currentDevice] systemVersion] integerValue] < 7)
+        return;
+    
+    self.extraColorLayer.backgroundColor = barTintColor.CGColor;
+}
+
+@end
